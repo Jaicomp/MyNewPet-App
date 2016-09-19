c97b2dfd4f7fba6a4609e5ebe06909665aa95749
Jaicomp <jaicompdeveloper@gmail.com>
2016-09-19 14:54:34 +0200

    Finished header (TODO real login)

--- web/public/js/header.js
+++ web/public/js/header.js
@@ -1,3 +1,14 @@
+
 function showLogin(login) {
 	document.getElementById('login').style.display = "block";
 }
+
+window.onclick = function(event) {
+	var login = document.getElementById('login');
+	if (event.target == login) {
+		login.style.display = "none";
+	}
+}
+
+
+
