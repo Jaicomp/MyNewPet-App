44b81f18d0fc26a5a723adb40c4e37515e2b1eb1
Jaicomp <jaicompdeveloper@gmail.com>
2016-09-25 20:30:48 +0200

    Added functionality to Filter

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,5 +1,25 @@
+function getText(textInput) {
 
+	var cards = document.getElementsByClassName('card');
+	if (textInput.value.length == 0) {
+		console.log("OKI");
+		for (var i = 0; i < cards.length; i++) {
+			cards[i].style.display = "block";
+		}
+		return;
+	}
 
+	for (var i = 0; i < cards.length; i++) {
+		if (!(cards[i].childNodes[1].childNodes[0].innerHTML == textInput.value)) {
+			cards[i].style.display = "none";
+
+		} else {
+			cards[i].style.display = "block";
+		}
+		
+	}
+
+}
 
 
 
