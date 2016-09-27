b566409d00dfbdbc3ff9d2518033d7aad57982dd
Jaicomp <jaicompdeveloper@gmail.com>
2016-09-27 17:30:03 +0200

    Now the app send register emails

--- web/public/js/header.js
+++ web/public/js/header.js
@@ -13,20 +13,50 @@ function signUpContent() {
 }
 
 function sendEmailSignUp() {
-	var email = document.getElementById("emailRegister");
+	var email = document.getElementById('emailRegisterInput');
+	var title = document.getElementById('emailRegisterTitle');
+	var registerButton = document.getElementById('registerButtonSignUp');
+ 
 	if (email.value.length > 0) {
 			var xmlhttp = new XMLHttpRequest();
 			xmlhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
-					window.alert(this.responseText);
+					if (this.responseText != 'FAIL') {
+						title.innerHTML = 'Introduce el ';
+						var wordCode = document.createTextNode('código ');
+						var lastWords = document.createTextNode('envia a tu correo electrónico: ');
+
+						var textLastWords = document.createElement('span');
+						textLastWords.style.color = 'black';
+						textLastWords.style.fontWeight = 'normal';
+						textLastWords.appendChild(lastWords);					
+
+						var text = document.createElement('span');
+						
+						text.style.color = 'red';
+						text.style.fontWeight = 'bold';
+						text.appendChild(wordCode);
+						text.appendChild(textLastWords);
+						title.appendChild(text);
+
+						email.value = '';
+						email.setAttribute('placeholder', 'Código');
+
+						registerButton.setAttribute('onclick', 'sendCodeSignUp()');
+					}
 				}
 		}
-		xmlhttp.open("GET", "sendemail", true);
+		xmlhttp.open('GET', 'sendemail?email='+email.value, true);
 		xmlhttp.send();
 	} else {
-		window.alert("Correo electrónico no válido, vuelvalo a intentar.");
+		window.alert('Correo electrónico no válido, vuelvalo a intentar.');
 	}
 }
+
+function sendCodeSignUp() {
+
+}
+
 
 window.onclick = function(event) {
 	var login = document.getElementById('login');
--- web/server.js
+++ web/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var pgp = require('pg-promise')();
+var helper = require('sendgrid').mail;
 
 var settings = require('./settings');
 var db = require('./core/db');
 var typeAnimal = require('./controllers/typeanimal');
 var bleed = require('./controllers/bleed');
 
-
 var app = express();
-
+var helper = require('sendgrid').mail;
 
 //Public assets
 app.use('/public', express.static(__dirname + '/public'));
@@ -27,9 +27,33 @@ app.get('/bleed', function(req, res){
 });
 
 app.get('/sendemail', function(req, res) {
-	
-	res.send("OKIO");
 
+	if ((req.query.email.length> 0) && /[a-zA-Z0-9!@]*@[a-zA-Z0-9!@]*/.test(req.query.email)) {
+		var from_email = new helper.Email('NuevaCuenta@MyNewPet.es');
+		var to_email = new helper.Email(req.query.email);
+		var subject = 'Bienvenido a MyNewPet!!';
+		var content = new helper.Content('text/plain', 'Bienvenido a MyNewPet!!\nBienvenido a tu nueva cuenta. Registrarte es muy fácil, solo tienes que introducir el siguiente código:');
+		var mail = new helper.Mail(from_email, subject, to_email, content);
+
+		var sg = require('sendgrid')('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');
+		var request = sg.emptyRequest({
+  		method: 'POST',
+  		path: '/v3/mail/send',
+  		body: mail.toJSON(),
+		});
+
+		sg.API(request, function(error, response) {
+			if (error) {
+				console.log (error);
+			}
+  		console.log(response.statusCode);
+  		console.log(response.body);
+  		console.log(response.headers);
+		});
+		res.send(req.query.email);
+	}
+
+	res.send('FAIL');
 });
 
 
